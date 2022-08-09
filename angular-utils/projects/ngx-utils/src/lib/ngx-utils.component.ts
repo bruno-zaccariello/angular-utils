d816ba2b361543d3ac8a324c3dc44e698d9b3d88
Bruno Zaccariello <brukzz90@gmail.com>
2022-08-09 20:26:33 -0300

    Initial Commit :)

---
+++ angular-utils/projects/ngx-utils/src/lib/ngx-utils.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-utils',
+  template: `
+    <p>
+      ngx-utils works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgxUtilsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
